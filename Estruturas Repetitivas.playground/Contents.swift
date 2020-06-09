31007d03cf6c26494e073ce6d5d98c5f582d7eea
Bruno Alves <alves.bruno945@gmail.com>
2020-06-09 17:07:39 -0300

    Repetitive Structures
    Repetitive Structures in Swift

---
+++ Estruturas Repetitivas.playground/Contents.swift
@@ -0,0 +1,21 @@
+// Neste documento vamos utilizar a estrutura de repiticao for
+
+for contador in 1...4 {
+    print("Hello World")
+}
+
+for dia in 1...4 {
+    print("Hoje é dia \(dia) de fevereiro")
+}
+
+let listaNome = ["Bruno","Joao", "Sergio", "Maria"]
+for nome in listaNome {
+    print("Nome: \(nome)")
+}
+
+// Estrutura de repeticao WHILE
+var lifes = 10
+while lifes > 0 {
+        print("O jogador está com \(lifes) restante")
+        lifes = lifes - 1
+}
